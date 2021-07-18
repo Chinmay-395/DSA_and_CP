8d48cdf6aff43423fa6e96634a7bbfeb2d1b986f
Chinmay-395 <chinmaydali395@gmail.com>
2021-07-18 20:48:28 +0530

    The multiple inheritance using interfaces

--- _C#_interivew_problem_practice/practice_C#/interface/multiple_inheritance.cs
+++ _C#_interivew_problem_practice/practice_C#/interface/multiple_inheritance.cs
@@ -1,1 +1,50 @@
 using System;
+
+interface IA
+{
+    void AMethod();
+}
+class A : IA
+{
+    public void AMethod()
+    {
+        Console.WriteLine("AMethod");
+    }
+}
+
+interface IB
+{
+    void BMethod();
+}
+class B : IB
+{
+    public void BMethod()
+    {
+        Console.WriteLine("BMethod");
+    }
+}
+
+class AB : IA, IB
+{
+    A a = new A();
+    B b = new B();
+
+    public void AMethod()
+    {
+        a.AMethod();
+    }
+    public void BMethod()
+    {
+        b.BMethod();
+    }
+}
+
+class Program
+{
+    public static void Main()
+    {
+        AB abObj = new AB();
+        ABObj.AMethod();
+        ABObj.BMethod();
+    }
+}
