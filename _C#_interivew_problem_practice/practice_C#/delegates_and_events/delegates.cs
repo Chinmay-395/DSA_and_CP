bd4b55c597301a3b3b92f9f02c82f9655dbe71fe
Chinmay395 <chinmaydali395@gmail.com>
2022-06-17 13:37:41 +0530

    Deleting unnecessary files

--- _C#_interivew_problem_practice/practice_C#/delegates_and_events/delegates.cs
+++
@@ -1,8 +0,0 @@
-/*
-    A delegate is a type safe function pointer
-        It is a contract between two methods/objects/etc
-        Determines the signature of the event
-    A delegate is mainly used for "callback"
-*/
-using System;
-// class 
--- hackerrack-problems/DS/01-Array/01_hourglass_problem/note.md
+++
@@ -1,7 +0,0 @@
-```
-[i,j]  - [i,j+1]  -[i,j+2]
-       - [i+1,j+1]-
-[i+2,j]- [i+2,j+1]-[i+2,j+2]
-```
-
-NOTE: I have to travel horizontally & vertically only 4 iterations
