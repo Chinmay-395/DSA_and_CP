6ad983a01fd3d51e9ca9ef22a09dac47919f4488
chinmay395 <chinmaydali395@gmail.com>
2023-07-21 11:38:38 -0400

    moved all the problem attempts into one folder

--- new.cpp
+++ new.cpp
@@ -1,28 +1,18 @@
 // #include <stdio.h>
 #include <bits/stdc++.h>
-// using namespace std;
-int foo()
-{
-    int x = 0;
-    int ans = x + (x = 1);
-    // std::cout << ans << std::endl;
-    return ans;
-}
+using namespace std;
+
 int main()
 {
-    std::cout << foo();
-    return 0;
+  printf("enter full name \t");
+  string s;
+  getline(cin, s);
+  cout << "the entire s" << endl
+       << s;
+  stringstream ss(s);
+  cout << "The entire vector" << endl;
+  // for(auto a: x){
+  //   cout<<a<<endl;
+  // }
+  return 0;
 }
-
-/*
-let x = 1 in
-  let f = fun y -> x in
-  let x = 2 in
-  f 0;;
-
-  let x = 1 in
-  let x = 2 in
-  let f = fun y -> x in
-  f 0;;
-
-*/
